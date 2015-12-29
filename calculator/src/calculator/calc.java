de3f030cd54d5100bb478d6581058b475973e78b
goblueapple <lxk_zju@hotmail.com>
2015-12-29 18:55:25 -0500

    good

---
+++ calculator/src/calculator/calc.java
@@ -0,0 +1,83 @@
+package calculator;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class calc {
+
+	protected Shell shell;
+	private Text text;
+	private Text text_1;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			calc window = new calc();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		text = new Text(shell, SWT.BORDER);
+		text.setBounds(47, 26, 71, 21);
+		
+		Button btnNewButton = new Button(shell, SWT.NONE);
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(153, 26, 75, 25);
+		btnNewButton.setText("New Button");
+		
+	
+		
+		text_1 = new Text(shell, SWT.BORDER);
+		text_1.setBounds(47, 63, 71, 21);
+		
+		
+		Button btnNewButton_1 = new Button(shell, SWT.NONE);
+		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+		     String kdkd = "484";
+			}
+		});
+		btnNewButton_1.setBounds(153, 61, 75, 25);
+		btnNewButton_1.setText("New Button");
+
+	}
+}
